0a7c48b286d573165f7b0f8a9faa8e4ec28abc26
shivam saxena <shivamsaxenagr8@gmail.com>
2023-06-17 15:38:37 +0530

    added pricing, working on contact form

--- config.php
+++ config.php
@@ -1,20 +1,26 @@
 <?php
-$servername = "localhost";
-$username = "u226556951_shivam";
-$password = "D5j;n#PQ";
-$dbname = "u226556951_contact";
+define("DB_HOST", "localhost");
+define("DB_USER", "u226556951_shivam");
+define("DB_PASSWORD", "D5j;n#PQ");
+define("DB_DATABASE", "u226556951_contact");
 
-// Create a connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE)
+// $servername = "localhost";
+// $username = "u226556951_shivam";
+// $password = "D5j;n#PQ";
+// $dbname = "u226556951_contact";
 
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// // Create a connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Connection successful
-echo "Connected successfully";
+// // Check the connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
-// Close the connection
-$conn->close();
+// // Connection successful
+// echo "Connected successfully";
+
+// // Close the connection
+// $conn->close();
 ?>
