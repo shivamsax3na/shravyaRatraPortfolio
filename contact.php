3ce1e513147781059b7a475f80420c5072d9d62d
shivam saxena <shivamsaxenagr8@gmail.com>
2023-06-17 14:37:01 +0530

    added pricing, working on contact form

--- contact.php
+++ contact.php
@@ -1,3 +1,12 @@
 <?php
-print_r($_POST);
+include("config.php");
+extract($_POST);
+$query = "INSERT INTO `contactForm` (`name`, `email`, `contact`, `message`, `companyName`) VALUES ('". $name .", ". $email .", ". $contact .", ". $message .", ". $companyName ."')";
+$result = $mysqli->query($query);    
+
+if(!$result) {
+    echo "Something went wrong".$mysqli->err;
+}
+echo "thanks we will get back to you";
+$mysqli->close();
 ?>
