60905c0d4b8fd059a1148a05974bdb470df5b38c
shivam saxena <shivamsaxenagr8@gmail.com>
2023-06-17 16:20:39 +0530

    added pricing, working on contact form

--- contact.php
+++ contact.php
@@ -1,12 +1,36 @@
 <?php
-include("config.php");
-extract($_POST);
-$query = "INSERT INTO `contactForm` (`name`, `email`, `contact`, `message`, `companyName`) VALUES ('". $name .", ". $email .", ". $contact .", ". $message .", ". $companyName ."')";
-$result = $mysqli->query($query);    
+$servername = "localhost";
+$username = "u226556951_shivam";
+$password = "D5j;n#PQ";
+$dbname = "u226556951_contact";
 
-if(!$result) {
-    echo "Something went wrong".$mysqli->err;
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-echo "thanks we will get back to you";
-$mysqli->close();
-?>
+
+// Retrieve the form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$companyName = $_POST['companyName'];
+$contact = $_POST['contact'];
+$message = $_POST['message'];
+
+// Prepare and execute the SQL statement to insert the form data into the database
+$stmt = $conn->prepare("INSERT INTO contactForm (name, email, company, phone, message) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sssss", $name, $email, $contact, $message, $companyName);
+$stmt->execute();
+
+// Check if the insertion was successful
+if ($stmt->affected_rows > 0) {
+    echo "Thank you for contacting us!";
+} else {
+    echo "Error: Unable to submit the form. Please try again later.";
+}
+
+//
+
+
